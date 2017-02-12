f90bcde8ceef9b75f8e1b93e8a55e822b7eeffaf
Jon Brookes <marshyon@gmail.com>
2017-02-12 16:50:12 +0000

    added template level subscription

---
+++ client/main.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+import './main.css';
+var graph = require('./graph.js');
+import Chart from 'chart.js'
+
+Template.hello.onCreated(function() {
+  this.autorun(() => {
+    this.subscribe('NumberData');
+  });
+});
+
+Template.hello.helpers({  
+    'NumberData': function(){
+        return NumberData.find()
+    }
+});
+
+Template.hello.onRendered(function() {
+    Deps.autorun(function() {
+      graph.drawChart()
+    });
+});
